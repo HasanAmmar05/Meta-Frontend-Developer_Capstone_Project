ae2675c30cdafb5d6f22cff626e173cb9153a064
HasanAmmar05 <altalib.hasan05@gmail.com>
2024-09-12 18:13:07 +0800

    Write the footer and we are done

--- README.md
+++ README.md
@@ -13,3 +13,35 @@ Here are some screeenshots of the application showcasing the Booking functionali
 ![little lemon website table booking](/src/images/github-cover.png)
 
 
+
+
+import React from "react";
+import logo from "../images/small_logo.png";
+const Footer = () => {
+  return (
+    <footer>
+      <section>
+        <div className="company-info">
+          <img src={logo} alt=""></img>
+          <p>
+            We are a family Mediteranian restaurant, focused on traditional
+            recipes served with a modern twist.
+          </p>
+        </div>
+
+
+        <div>
+            <h3> Contact </h3>
+            <ul>
+                <li> Address: <br> 123 Towncity, USA</br></li>
+                <li> Phone: <br> +0123455678</br></li>
+                <li> Email: <br> littleLemon@gmail.com</br></li>
+
+            </ul>
+        </div>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,67 @@
-import React from 'react'
-
+import React from "react";
+import logo from "../images/small_logo.png";
 const Footer = () => {
   return (
     <footer>
+      <section>
+        <div className="company-info">
+          <img src={logo} alt=""></img>
+          <p>
+            We are a family Mediteranian restaurant, focused on traditional
+            recipes served with a modern twist.
+          </p>
+        </div>
+        <div>
+          <h3> Important Links </h3>
+          <ul>
+            <li>
+              <a href="/"> Home </a>
+            </li>
+            <li>
+              <a href="/"> About </a>
+            </li>
+            <li>
+              <a href="/"> Menu </a>
+            </li>
+            <li>
+              <a href="/"> Reservations </a>
+            </li>
+            <li>
+              <a href="/"> Order Online </a>
+            </li>
+            <li>
+              <a href="/"> Login </a>
+            </li>
+          </ul>
+        </div>
 
+        <div>
+          <h3> Contact</h3>
+          <ul>
+            <li> Address: 123 Town City, USA</li>
+            <li> Phone: +012345678</li>
+            <li> Email: littleLemon@gmail.com</li>
+          </ul>
+        </div>
+
+
+        <div>
+            <h3> Socials</h3>
+            <ul>
+            <li>
+              <a href="/"> Facebook </a>
+            </li>
+            <li>
+              <a href="/"> Instagram </a>
+            </li>
+            <li>
+              <a href="/"> Twitter</a>
+            </li>
+          </ul>
+        </div>
+      </section>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
